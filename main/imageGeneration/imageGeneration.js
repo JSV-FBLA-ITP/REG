7b0da00ba78460de2ec8012a32b644b4d62c964c
JAI-THE-GUY <jai.mokashi@gmail.com>
2026-01-04 11:01:41 -0600

    everything ready for hugging face ai generation

--- main/imageGeneration/imageGeneration.js
+++
@@ -1,6 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-
-/* --- SETUP AI --- */
-const API_KEY = "AIzaSyBjPG57WFi9iZU2lBPZj8uPLkym_hzG77s"; 
-const genAI = new GoogleGenAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-image" });
